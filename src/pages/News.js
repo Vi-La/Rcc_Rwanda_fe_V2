0759f668d7957765c02d04d2cf37bba9c47a9dd3
wakaflorien <waka.florien45@gmail.com>
2022-07-25 18:15:52 +0200

    adds news layout

--- src/pages/News.js
+++ src/pages/News.js
@@ -1,12 +1,29 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import newsDummy from '../utils/dummyNews.json'
 
 const News = () => {
+    console.log("dummy data",newsDummy)
     return (
         <>
             <Navbar />
-            <div className='text-7xl py-3 font-serif text-center'>
-                <h1>News Coming soon</h1>
+            <div className="flex flex-col px-40 py-10">
+                {
+                    newsDummy.map((news, index) => {
+                        return(
+                            <div key={index} className="flex flex-row py-10 justify-center">
+                                <div className="flex flex-col pr-10">
+                                <img src={news.img} />
+                                <span className="mt-2">Read more ...</span>
+                                </div>
+                                <div className="flex flex-col">
+                                    <h1>Description</h1>
+                                    <p>{news.description}</p>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
             </div>
         </>
     )
