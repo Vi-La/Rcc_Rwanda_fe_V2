ac9476e6b8ff8a457202de1f34d65e9731574839
wakaflorien <waka.florien45@gmail.com>
2022-07-28 00:03:19 +0200

    adds pagination

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+// import prev from '../../assets/svgs/prev.svg';
+// import next from '../../assets/svgs/next.svg';
+
+const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
+    const pageNumbers = []
+    for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+        pageNumbers.push(i);
+      }
+    return ( 
+        <div className="w-full flex items-center justify-center pb-10">
+            <div className="p-1 px-4 shadow flex mt-3">
+                {pageNumbers.map( number => (
+                <div key={number} onClick={() => paginate(number)} className="text-black text-2xl hover:flex hover:items-center hover:justify-center hover:rounded-md cursor-pointer hover:bg-primary-400 hover:w-8 hover:text-gray-400 px-4">{number}</div>
+                ))}
+            </div>
+        </div>
+     );
+}
+export default Pagination;
--- src/pages/News.js
+++ src/pages/News.js
@@ -1,30 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../components/Navbar'
+import Pagination from '../components/Pagination'
 import newsDummy from '../utils/dummyNews.json'
 
 const News = () => {
-    console.log("dummy data",newsDummy)
+    const [currentPage, setCurrentpage] = useState(1)
+    const [postsPerPage] = useState(3)
+
+    const indexOfLastPost = currentPage * postsPerPage;
+    const indexOfFirstPost = indexOfLastPost - postsPerPage;
+    const currentPosts = newsDummy.slice(indexOfFirstPost, indexOfLastPost);
+
+    const paginate = pageNumber => setCurrentpage(pageNumber)
     return (
         <>
             <Navbar />
             <div className="flex flex-col px-40 py-10">
+                <h1 className="text-3xl text-bold ">Latest news</h1>
+                <hr className="mt-3"/>
                 {
-                    newsDummy.map((news, index) => {
+                    currentPosts.map((news, index) => {
                         return(
-                            <div key={index} className="flex flex-row py-10 justify-center">
+                            <div key={index} className="flex flex-row py-10">
                                 <div className="flex flex-col pr-10">
-                                <img src={news.img} />
-                                <span className="mt-2">Read more ...</span>
+                                    <img src={news.img} className="w-72 rounded-sm shadow-xl" />
                                 </div>
                                 <div className="flex flex-col">
-                                    <h1>Description</h1>
-                                    <p>{news.description}</p>
+                                    <h1 className="text-2xl text-bold ">{news.title}</h1>
+                                    <p className="mt-3 mb-3 w-1/2 text-md">{news.description}</p>
+                                    <a href="#"><span className="cursor-pointer hover:text-blue-800 text-xl">Read more ...</span></a>
                                 </div>
                             </div>
                         )
                     })
                 }
             </div>
+            <Pagination
+                postsPerPage={postsPerPage}
+                totalPosts={newsDummy.length}
+                paginate={paginate}
+            />
         </>
     )
 }
